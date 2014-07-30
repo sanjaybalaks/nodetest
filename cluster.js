0572c50abcd012988f7060178b4c0044dc50c943
sanjay <sanjaybalak@gmail.com>
2014-07-30 22:44:19 +0530

    changed

--- cluster.js
+++ cluster.js
@@ -2,18 +2,7 @@ var cluster = require('cluster');
 var http = require('http');
 var numCPUs = require('os').cpus().length;
 
-if (cluster.isMaster) {
-  process.stdout.write('Starting worker ... ')
-  // Fork workers.
-  for (var i = 0; i < numCPUs; i++) {
-    console.log('fork');
-    cluster.fork();
-  }
 
-  cluster.on('exit', function(worker, code, signal) {
-    console.log('worker ' + worker.process.pid + ' died');
-  });
-} else {
   // Workers can share any TCP connection
   // In this case its a HTTP server
   http.createServer(function(req, res) {
@@ -21,4 +10,3 @@ if (cluster.isMaster) {
     res.end("hello world\n");
     console.log('node cluster activated');
   }).listen(8000);
-}
